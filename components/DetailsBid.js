953b97158b33e26da90fe85c1235062e0288f25c
Yann Cotan <Yankolamborghinie3@gmail.com>
2023-01-27 02:04:41 +0000

    Finished first react native project

---
+++ components/DetailsBid.js
@@ -0,0 +1,50 @@
+import { View, Text, Image } from 'react-native'
+
+import { EthPrice } from './SubInfo'
+import {COLORS , SIZES ,FONTS} from '../constants'
+
+
+const DetailsBid = ({bid}) => {
+  return (
+    <View style={{
+      width: '100%',
+      flexDirection: "row",
+      justifyContent: "space-between",
+       alignItems: "center",
+       marginVertical: SIZES.base,
+       paddingHorizontal: SIZES.base * 2,
+    
+    }}>
+      <Image
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 40, height: 40}}
+      />
+      <View>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary
+          }}
+        >
+          Bid place by {bid.name}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.secondary,
+            marginTop: 3
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
+
+      <EthPrice price={bid.price}/>
+    </View>
+  )
+}
+
+export default DetailsBid
---
+++ components/index.js
@@ -0,0 +1,12 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton} from "./Button";
+import { SubInfo , EthPrice, NFTTitle} from "./SubInfo";
+import  DetailsBid from './DetailsBid'
+import  DetailsDesc from './DetailsDesc'
+
+export {
+    HomeHeader , NFTCard , FocusedStatusBar, CircleButton, RectButton,
+    SubInfo, EthPrice, NFTTitle, DetailsBid, DetailsDesc
+}
